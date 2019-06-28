17ec20f119f4404e51b457103e0fb76ee928f7c3
jpikavalko <jpikavalko@gmail.com>
2019-06-28 13:15:13 +0300

    exercise_03_beautifullTriangles.js Added FOR REAL

--- exercise_03_beautifullTriangles.js
+++ exercise_03_beautifullTriangles.js
@@ -4,8 +4,59 @@ window.onload = function () {
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight;
 
-    
+    var triangles = [];
+    var Ax, Ay, Bx, By, Cx, Cy, Dx, Dy, Radius, Area, ColorR, ColorG, ColorB, ColorA;
+    var angle = 0;
 
-        
-        requestAnimationFrame(updateTriangles);
+    CreateTriangle();
+    UpdateTriangles();
+
+    function CreateTriangle()
+    {
+        // Calculate variables
+        Ax = width/2 - 50;
+        Ay = height/2 - 50;
+        Bx = width/2 + 50;
+        By = height/2 - 50;
+        Cx = width/2;
+        Cy = height/2 + 50;
+        Dx = 0;
+        Dy = 0;
+
+        // Create points (not needed here actually)
+        context.beginPath();
+        context.moveTo(Ax, Ay);
+        context.lineTo(Bx, By);
+        context.lineTo(Cx, Cy);
+        context.lineTo(Ax, Ay);
+        context.stroke();
+    }
+
+    // Update Triangles position and color
+    function UpdateTriangles()
+    {
+        Ax = width/2 + Math.cos(angle) * 20;
+        Ay = height/2 + Math.sin(angle) * 60;
+        Bx = width/2 + Math.cos(angle+20) * 60;
+        By = height/2 + Math.sin(angle+20) * 180;
+        Cx = width/2 + Math.cos(angle+40) * 40;
+        Cy = height/2 + Math.sin(angle+40) * 120;
+
+        context.clearRect(0, 1, width, height);
+        context.beginPath();
+        context.moveTo(Ax, Ay);
+        context.lineTo(Bx, By);
+        context.lineTo(Cx, Cy);
+        context.lineTo(Ax, Ay);
+        context.stroke();
+
+        angle += 0.01;
+
+        requestAnimationFrame(UpdateTriangles);
+    }
+
+    function CalculateMiddlePointOf()
+    {
+
+    }
 }
